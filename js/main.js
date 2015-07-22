5b33413ef4e635f0e91e6835081dde549cada0d2
Martin Greenwood <martin@pixelpudu.com>
2015-07-22 06:56:40 +0100

    WYSIWYG Toolbar
    Updates to wysiwyg editor toolbar.

--- js/main.js
+++ js/main.js
@@ -15,14 +15,5 @@ $(function() {
 		height: 300,                 // set editor height
 		minHeight: null,             // set minimum height of editor
 		maxHeight: null,             // set maximum height of editor
-
-		toolbar: [
-			['style', ['bold', 'italic', 'underline', 'clear']],
-		    ['font', ['strikethrough', 'superscript', 'subscript']],
-		    ['fontsize', ['fontsize']],
-		   	['color', ['color']],
-	    	['para', ['ul', 'ol', 'paragraph']],
-		    ['height', ['height']],
-		]
 	});
 });
